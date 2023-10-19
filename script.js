b340d12f8241255b9009a73f89f0c8b4c2ef2f0e
srihanth-m <srishanthmule@gmail.com>
2023-10-19 14:04:10 +0530

    project done

---
+++ script.js
@@ -0,0 +1,26 @@
+const item = document.querySelector("#item");
+const todo = document.querySelector("#to-do-box");
+
+item.addEventListener("keyup" , function(event){
+    if(event.key == "Enter"){
+        addTodo(this.value);
+        this.value = "";
+    }
+})
+
+const addTodo = (item) =>{
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+        ${item};
+        <i class="fas fa-times"></i>
+    `;
+
+    listItem.addEventListener("click" , function(){
+        this.classList.toggle("done");
+    })
+
+    listItem.querySelector("i").addEventListener("click" , function(){
+        listItem.remove();
+    })
+    todo.appendChild(listItem);
+}
